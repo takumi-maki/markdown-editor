d81ecf183a160c580ae6ccae59d141c57a7f4d33
takumiiimo <assistant2370@gmail.com>
2021-05-24 14:36:03 +0900

    保存機能、ページング機能の実装

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -5,29 +5,11 @@ import * as ReactMarkdown from 'react-markdown'
 import { putMemo } from '../indexeddb/memos'
 import { Button } from '../components/button'
 import { SaveModal } from '../components/save_modal'
+import { Link } from 'react-router-dom'
+import { Header } from '../components/header'
 
 const { useState } = React
 
-
-const Header = styled.header`
-    align-content: center;
-    display: flex;
-    font-size: 1.5rem;
-    height: 2rem;
-    justify-content: space-between;
-    left: 0;
-    line-height: 2rem;
-    padding: 0.5rem 1rem;
-    position: fixed;
-    right: 0;
-    top: 0;
-`
-
-const HeaderControl = styled.div`
-    height: 2rem;
-    display: flex;
-    align-content: center;
-`
 
 const Wrapper = styled.div`
     bottom: 0;
@@ -35,6 +17,13 @@ const Wrapper = styled.div`
     position: fixed;
     right: 0;
     top: 3rem;
+`
+
+const HeaderArea = styled.div`
+    position: fixed;
+    right: 0;
+    top: 0;
+    left: 0;
 `
 
 const TextArea = styled.textarea`
@@ -60,40 +49,42 @@ const Preview = styled.div`
     width: 50vw;
 `
 
-const StorageKey = 'pages/editor:text'
+interface Props {
+    text: string
+    setText: (text: string) => void
+}
 
-export const Editor: React.FC = () => {
-    const [text, setText] = useStateWithStorage('', StorageKey)
-
+export const Editor: React.FC<Props> = (props) => {
+    const { text, setText } = props
     const [showModal, setShowModal] = useState(false)
 
     return (
         <>
-            <Header>
-                Markdown Editor
-                <HeaderControl>
+            <HeaderArea>
+                <Header title="Markdown Editor">
                     <Button onClick={() => setShowModal(true)}>保存する</Button>
-                </HeaderControl>
-            </Header>
+                    <Link to="/history">履歴を見る</Link>
+                </Header>
+            </HeaderArea>
             <Wrapper>
-
                 <TextArea onChange={(event) => setText(event.target.value)}
                     value={text}
                 />
-
                 <Preview>
                     <ReactMarkdown>{text}</ReactMarkdown>
                 </Preview>
             </Wrapper>
-            {showModal && (
-                <SaveModal
-                    onSave={(title: string): void => {
-                        putMemo(title, text)
-                        setShowModal(false)
-                    }}
-                    onCancel={() => setShowModal(false)}
-                />
-            )}
+            {
+                showModal && (
+                    <SaveModal
+                        onSave={(title: string): void => {
+                            putMemo(title, text)
+                            setShowModal(false)
+                        }}
+                        onCancel={() => setShowModal(false)}
+                    />
+                )
+            }
         </>
 
     )
